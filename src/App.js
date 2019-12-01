97561ef1d344875386701616f6c699c52efa048f
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-01 10:59:52 +0300

    add Profile in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import Profile from "./Profile";
 
 function App() {
   return (
     <div className="App">
      <div className="portfolio">
        <Header/>
+       <Profile/>
      </div>
     </div>
   );
---
+++ src/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Profile.module.css'
+
+function Profile() {
+    return (
+        <div className={style.profile}>
+        </div>
+    );
+}
+
+export default Profile;
