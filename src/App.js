64e8d4c9af2061b269d81a8537a9cd191c4d7bc9
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-01 10:56:04 +0300

    add Header in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-     <div className="portfolio"></div>
+     <div className="portfolio">
+       <Header/>
+     </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import style from './Header.module.css'
 
 function Header() {
     return (
-        <div className="App">
+        <div className={style.header}>
         </div>
     );
 }
