acd3d91f9e6fb0eb6876ce54c645c9b385a5dc1b
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-01 11:57:57 +0300

    add Link in Header

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,12 @@ import style from './Header.module.css'
 function Header() {
     return (
         <div className={style.header}>
+            <div className="nav">
+                <a href="" className="link">Главная</a>
+                <a href="" className="link">Скиллы</a>
+                <a href="" className="link">Мои работы</a>
+                <a href="" className="link">Контакты</a>
+            </div>
         </div>
     );
 }
