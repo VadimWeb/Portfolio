e62bfed89c091a62d5b61c23a352472e228efa08
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-01 11:12:36 +0300

    add Works in App

---
+++ src/Works.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Works.module.css'
+
+function Works() {
+    return (
+        <div className={style.works}>
+        </div>
+    );
+}
+
+export default Works;
