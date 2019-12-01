05436619020a2a28662147f7dfc7bfa2c5ec6d76
VadimWeb <vad.cudin2011@yandex.ru>
2019-12-01 11:08:25 +0300

    add Skills in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Profile from "./Profile";
+import Skills from "./Skills";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <div className="portfolio">
        <Header/>
        <Profile/>
+       <Skills/>
      </div>
     </div>
   );
---
+++ src/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Skills.module.css'
+
+function Skills() {
+    return (
+        <div className={style.skills}>
+        </div>
+    );
+}
+
+export default Skills;
